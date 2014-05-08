aaeb1b17c76a30d6597d4f01eae3a9484a6fc23e
Cong <congusbongus@gmail.com>
2014-05-08 12:49:53 +1000

    Add DrumDefs
    Drums differentiated by drum type (e.g. bass drum, snare)

--- scripts/drum.js
+++ scripts/drum.js
@@ -1,10 +1,35 @@
 var ALPHA = 0.3;
-var Drum = function(game, grid, sound) {
+var DrumDef = function(name, numbers) {
+  this.name = name;
+  this.numbers = numbers;
+  this.filename = function(i) {
+    return "audio/78-" + name + numbers[i] + ".mp3";
+  };
+  this.getRandomName = function() {
+    return name + numbers[Math.floor(Math.random() * numbers.length)];
+  };
+}
+var DrumDefs = {
+  BD:  new DrumDef('BD',  [1, 2, 3]),           // bass drum
+  BHI: new DrumDef('BHI', [1, 3]),              // bongos
+  BLO: new DrumDef('BLO', [1, 3]),
+  BME: new DrumDef('BME', [1, 2, 3]),
+  CLA: new DrumDef('CLA', [1, 2]),              // claves
+  COW: new DrumDef('COW', [1, 2]),              // cowbell
+  GUI: new DrumDef('GUI', [1, 2, 3, 4, 5, 6]),  // guiro
+  HH:  new DrumDef('HH',  [1, 2, 3, 4]),        // hihat
+  HO:  new DrumDef('HO',  [1, 2, 3, 4]),        // ?
+  ME:  new DrumDef('ME',  [1, 2, 3, 4]),        // "metal beat"
+  RIM: new DrumDef('RIM', [1, 2, 3]),           // rimshot
+  SD:  new DrumDef('SD',  [1, 2, 3, 4]),        // snare drum
+  TAM: new DrumDef('TAM', [1, 2, 3])            // tambourine
+};
+var Drum = function(game, grid, drumdef) {
   //Sprite
   this.pos = g2p(grid);
   Phaser.Sprite.call(this, game, this.pos.x, this.pos.y, 'dot');
   this.alpha = ALPHA;
-  this.sound = game.add.audio(sound);
+  this.sound = game.add.audio(drumdef.getRandomName());
   this.timer = game.time;
   this.timeLast = this.timer.now;
 }
